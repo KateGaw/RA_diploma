ecd6a19f1cf180b767e1a8405746a9416444f701
AppVeyor <ci@appveyor.com>
2020-07-22 07:41:36 +0000

    023b794

---
+++ precache-manifest.79d4618c1094ce00189383ec9bc54c18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82231ab7b05dadc5dc1b8add06eb24cd",
+    "url": "/RA_diploma/index.html"
+  },
+  {
+    "revision": "375e855085c9699e751d",
+    "url": "/RA_diploma/static/css/main.afb2d2cc.chunk.css"
+  },
+  {
+    "revision": "097d4924cf1178b35054",
+    "url": "/RA_diploma/static/js/2.ea3ff833.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RA_diploma/static/js/2.ea3ff833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375e855085c9699e751d",
+    "url": "/RA_diploma/static/js/main.e9f4c61b.chunk.js"
+  },
+  {
+    "revision": "c5c705d9e256932773ef",
+    "url": "/RA_diploma/static/js/runtime-main.c205ae3b.js"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/RA_diploma/static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "/RA_diploma/static/media/footer-sprite.9c087d43.png"
+  },
+  {
+    "revision": "3d0086d238b116794eccb46a0e196525",
+    "url": "/RA_diploma/static/media/noimage.3d0086d2.png"
+  }
+]);
